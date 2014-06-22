6f748a83fa5d377a3806806a559c05756a223ff4
Zach Metcalf <zachmetcalf@gmail.com>
2014-06-22 10:46:54 -0700

    Updates to Mac and Linux Projects

---
+++ Mac/Build/Intermediates/Client.build/Debug/Client.build/Objects-normal/x86_64/ClientMain.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/zachary/Desktop/Dropbox/Cross/TCPSource/Demos/ClientMain.cpp \
+  ../../TCPLibrary/Inc/TCPLibrary.h ../../TCPLibrary/Inc/TCPBuild.h \
+  ../../TCPLibrary/Inc/TCPSocket.h ../../TCPLibrary/Inc/TCPSocketUtil.h \
+  ../../TCPLibrary/Inc/Proxy.h ../../TCPLibrary/Inc/Server.h \
+  ../../TCPLibrary/Inc/Client.h
